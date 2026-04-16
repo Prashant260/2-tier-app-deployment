c08aae94cf8b0eb5f6607cf59c7dbf345ef1cce5
prashant260 <prashantagchoudhary2004@gmail.com>
2026-04-16 14:32:21 +0000

    changed app

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data')
-def get_data():
-    return jsonify({
-        "message": "Hello from Backend 🚀"
-    })
+@app.route('/api/message', methods=['POST'])
+def message():
+    data = request.get_json()
+    user_message = data.get('message')
 
+    response = f"Server received: {user_message}"
+
+    return jsonify({"response": response})
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from backend!"})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
