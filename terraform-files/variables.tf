92dec3390c6ddc5946ce36c2a5292c58723aefdd
prashant260 <prashantagchoudhary2004@gmail.com>
2026-04-20 13:42:45 +0000

    changed the instancetype

--- terraform-files/variables.tf
+++ terraform-files/variables.tf
@@ -1,7 +1,7 @@
 variable "environments" {
   default = {
     dev = {
-      instance_type = "t2.micro"
+      instance_type = "t3.small"
     }
     staging = {
       instance_type = "c7i-flex.large"
