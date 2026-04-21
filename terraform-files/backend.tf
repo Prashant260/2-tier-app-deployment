38957c8b9d56ec426650700e0aea1802ba4a2e58
prashant260 <prashantagchoudhary2004@gmail.com>
2026-04-21 08:01:07 +0000

    fixed backend.tf/

--- terraform-files/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "prashant-terraform-state-bucket-12345"
-    key            = "two-tier-app/${var.env}/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
